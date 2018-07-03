920854710aff6d839598e3d84e5805d8d568097c
Reza Pramudhika <rezapramudhika@gmail.com>
2018-07-03 19:17:03 +0700

    Add cashier registration

--- management/controllers/registration.js
+++ management/controllers/registration.js
@@ -13,18 +13,49 @@ module.exports = {
             created_at: new Date().toDateString(),
             updated_at: new Date().toDateString(),
             RoleId: 1
-          })
+        })
             .then(data => {
-              res.status(201).json({
-                  msg: 'Success',
-                  data
-              });
+                res.status(201).json({
+                    msg: 'Success',
+                    data
+                });
             })
             .catch(err => {
-              console.log(err);
-              res.status(500).json({
-                  msg: 'Internal Server Error'
-              })
+                console.log(err);
+                res.status(500).json({
+                    msg: 'Internal Server Error'
+                })
+            });
+    },
+    registerCashier: (req, res) => {
+        let salt = bcrypt.genSaltSync(saltRounds);
+        let hash = bcrypt.hashSync(req.body.password, salt);
+        model.User.create({
+            email: req.body.email,
+            password: hash,
+            name: req.body.name,
+            created_at: new Date().toDateString(),
+            updated_at: new Date().toDateString(),
+            RoleId: 2
+        })
+            .then(data => {
+                model.UserRestaurant.create({
+                    UserId: data.id,
+                    RoleId: data.RoleId,
+                    RestaurantId: req.query.restaurant_id
+                }).then(conjuction => {
+                    res.status(201).json({
+                        msg: 'Success',
+                        conjuction,
+                        data
+                    });
+                })
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    msg: 'Internal Server Error'
+                })
             });
     },
     getUser: (req, res) => {
@@ -37,12 +68,12 @@ module.exports = {
                 msg: 'Success',
                 data
             });
-          })
-          .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                msg: 'Internal Server Error'
-            })
-          });
+        })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    msg: 'Internal Server Error'
+                })
+            });
     }
 }
--- management/routes/registration.js
+++ management/routes/registration.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { register, getUser } = require('../controllers/registration')
+const { register, getUser, registerCashier } = require('../controllers/registration')
+const { loginAuth, isOwner } = require('../middlewares/auth');
 
 /* GET users listing. */
 router.post('/', register);
-router.get('/', getUser)
+router.get('/', getUser);
+router.post('/cashier', loginAuth, isOwner, registerCashier);
+
 module.exports = router;
